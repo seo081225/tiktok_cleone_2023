56bde658ebcd33a12fcbdc8df655fed718bc3cd6
seo <seo081225@gmail.com>
2023-09-15 02:59:53 +0900

    .

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -8,6 +8,7 @@
 
 import 'package:camera_web/camera_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:video_player_web/video_player_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -15,6 +16,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   CameraPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
   VideoPlayerPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -9,14 +9,19 @@ import 'dart:io'; // flutter_ignore: dart_io_import.
 import 'package:camera_android/camera_android.dart';
 import 'package:image_picker_android/image_picker_android.dart';
 import 'package:path_provider_android/path_provider_android.dart';
+import 'package:shared_preferences_android/shared_preferences_android.dart';
 import 'package:video_player_android/video_player_android.dart';
 import 'package:camera_avfoundation/camera_avfoundation.dart';
 import 'package:image_picker_ios/image_picker_ios.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
+import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
 import 'package:video_player_avfoundation/video_player_avfoundation.dart';
 import 'package:path_provider_linux/path_provider_linux.dart';
+import 'package:shared_preferences_linux/shared_preferences_linux.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
+import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
 import 'package:path_provider_windows/path_provider_windows.dart';
+import 'package:shared_preferences_windows/shared_preferences_windows.dart';
 
 @pragma('vm:entry-point')
 class _PluginRegistrant {
@@ -47,6 +52,15 @@ class _PluginRegistrant {
       } catch (err) {
         print(
           '`path_provider_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        SharedPreferencesAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_android` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -89,6 +103,15 @@ class _PluginRegistrant {
       }
 
       try {
+        SharedPreferencesFoundation.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_foundation` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         AVFoundationVideoPlayer.registerWith();
       } catch (err) {
         print(
@@ -107,6 +130,15 @@ class _PluginRegistrant {
         );
       }
 
+      try {
+        SharedPreferencesLinux.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_linux` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
     } else if (Platform.isMacOS) {
       try {
         PathProviderFoundation.registerWith();
@@ -117,12 +149,30 @@ class _PluginRegistrant {
         );
       }
 
+      try {
+        SharedPreferencesFoundation.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_foundation` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
     } else if (Platform.isWindows) {
       try {
         PathProviderWindows.registerWith();
       } catch (err) {
         print(
           '`path_provider_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        SharedPreferencesWindows.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_windows` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
